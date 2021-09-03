ad4007ef86f2cb47478f504bec07be0081eaa13d
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-09-02 20:18:20 -0500

    bug fix for api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,7 +5,7 @@ $( "#workOutBtn" ).click(function() {
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://exercisedb.p.rapidapi.com/exercises",
+        "url": "https://exercisedb.p.rapidapi.com/exercises/equipment/body%20weight",
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
@@ -25,7 +25,7 @@ $( "#nutriBtn" ).click(function() {
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C%20olives",
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=week&targetCalories=2000&exclude=shellfish%2C%20olives",
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
