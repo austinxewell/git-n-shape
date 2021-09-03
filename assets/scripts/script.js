d334daa44283d4f7a24444c4fd0ae8b8026176e8
AustinxEwell <austin.ewell86@gmail.com>
2021-09-02 20:20:07 -0600

    started function to pull 5 waist workouts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-// create variables for form input
+// create variables for exersiceDB
 var upperLegs = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/upper%20legs';
 var waist = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/waist';
 var lowerLegs = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/lower%20legs';
@@ -8,27 +8,58 @@ var chest = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest';
 var cardio = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio';
 var shoulders = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/shoulders';
 
-// waist (i = 0, i < 5, i++)
+
+// create variables for Recipe-Food-Nutrition
+
+
+
+//create function
+function startPage() {
+    $( "#workOutBtn" ).click(function() {
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": waist,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+                "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+            }
+        };
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+
+
+            
+        });
+      });
+}
+    
+
 
 
 
 //listen for form submit
-$( "#workOutBtn" ).click(function() {
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": waist, //"https://exercisedb.p.rapidapi.com/exercises/equipment/body%20weight",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-            "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-        }
-    };
+// $( "#workOutBtn" ).click(function() {
+//     const settings = {
+//         "async": true,
+//         "crossDomain": true,
+//         "url": "https://exercisedb.p.rapidapi.com/exercises/equipment/body%20weight",
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+//             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+//         }
+//     };
     
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-  });
+//     $.ajax(settings).done(function (response) {
+//         console.log(response);
+//     });
+//   });
+
+
+  
   
 
 $( "#nutriBtn" ).click(function() {
