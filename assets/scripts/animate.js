a8c203442da9a33ee3ae0509c03cd34c6a6dafed
SadboiTay <tg.hakes@gmail.com>
2021-09-05 15:41:36 -0600

    polish

--- assets/scripts/animate.js
+++ assets/scripts/animate.js
@@ -9,9 +9,9 @@ const sloganText = document.querySelector("#slogan");
 
 const tl = new TimelineMax();
 
-tl.fromTo(slider, 1.5, {height: "0%"}, {height: "65%", ease: Power2.easeInOut})
+tl.fromTo(slider, 1., {height: "0%"}, {height: "100%", ease: Power2.easeInOut})
 .fromTo(introText, 1.5, {opacity: 0}, {opacity: 1}, "-=.7")
-.fromTo(sloganText, 1.5, {opacity: 0, y: -50}, {opacity: 1, y: 0, ease: Power2.easeInOut}, "-=1")
+.fromTo(sloganText, 1, {opacity: 0, y: -50}, {opacity: 1, y: 0, ease: Power2.easeInOut}, "-=1")
 .fromTo(slider, 1, {x: "0%"}, {x: "-200%", ease: Power2.easeInOut}, "+=.5")
 .fromTo(dataSpace, 1.2, {x: "140%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
 .fromTo(formSpace, 1.2, {x: "500%"}, {x: "0%", ease: Power2.easeInOut}, "-=1")
