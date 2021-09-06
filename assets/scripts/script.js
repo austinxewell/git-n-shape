b426ce0bfe77ec9eed215047ed0c57a3c93273ed
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-09-06 15:49:55 -0500

    refactored workout script to reduce pulls. Currently randomizing 5 workouts. Working on appending to cards.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,12 @@
 // create variables for exersiceDB
-var upperLegs = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/upper%20legs';
-var waist = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/waist';
-var lowerLegs = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/lower%20legs';
-var back = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back';
-var cardio = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio';
-var chest = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest';
-var shoulders = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/shoulders';
-
+// var upperLegs = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/upper%20legs';
+// var waist = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/waist';
+// var lowerLegs = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/lower%20legs';
+// var back = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back';
+// var cardio = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio';
+// var chest = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest';
+// var shoulders = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/shoulders';
+var all = 'https://exercisedb.p.rapidapi.com/exercises/equipment/body%20weight';
 // DOM elements
 var workoutContainerEl = document.querySelector('#weekly-workout');
 var workoutCard = document.createElement('div');
@@ -15,57 +15,10 @@ var workoutCard = document.createElement('div');
 //5 upper legs workouts
 function startPage() {
     $("#workOutBtn").on('click', (function () {
-        for (i = 0; i < 5; i++) {
             const settings = {
                 "async": true,
                 "crossDomain": true,
-                "url": upperLegs,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-                    "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-                }
-            };
-
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            })
-            // append to dom element
-    
-            // workoutContainerEl.innerHTML = '';
-            // workoutCard.setAttribute(response[0]);
-            // workoutContainerEl.appendChild(workoutCard);
-
-
-        }
-
-
-
-        // 5 waist workouts
-        for (i = 0; i < 5; i++) {
-            const settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": waist,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-                    "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-                }
-            };
-
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            });
-        }
-
-
-        // 5 lower legs workouts
-        for (i = 0; i < 5; i++) {
-            const settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": lowerLegs,
+                "url": all,
                 "method": "GET",
                 "headers": {
                     "x-rapidapi-host": "exercisedb.p.rapidapi.com",
@@ -75,86 +28,138 @@ function startPage() {
 
             $.ajax(settings).done(function (response) {
                 console.log(response);
-            });
+                console.log(response.length);
+                
+                for(var i = 0; i < 5; i++){
+                    var workouts = (response[Math.floor(Math.random() * response.length)]);
+                console.log(workouts)
+                
+            }})
+
+            // append to dom element
+    
+            // workoutContainerEl.innerHTML = '';
+            // workoutCard.setAttribute(response[0]);
+            // workoutContainerEl.appendChild(workoutCard);
+
+
+        //for (i = 0; i < 5; i++) 
+
+
+        // 5 waist workouts
+        // for (i = 0; i < 5; i++) {
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": waist,
+        //         "method": "GET",
+        //         "headers": {
+        //             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        //             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+        //         }
+        //     };
+
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response[i]);
+        //     });
+        // }
+
+
+        // 5 lower legs workouts
+        // for (i = 0; i < 5; i++) {
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": lowerLegs,
+        //         "method": "GET",
+        //         "headers": {
+        //             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        //             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+        //         }
+        //     };
+
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response);
+        //     });
 
 
 
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            });
-        }
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response[i]);
+        //     });
+        // }
 
         // 5 back workouts
-        for (i = 0; i < 5; i++) {
-            const settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": back,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-                    "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-                }
-            };
+        // for (i = 0; i < 5; i++) {
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": back,
+        //         "method": "GET",
+        //         "headers": {
+        //             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        //             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+        //         }
+        //     };
 
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            });
-        }
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response[i]);
+        //     });
+        // }
 
         // 5 cardio workouts
-        for (i = 0; i < 5; i++) {
-            const settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": cardio,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-                    "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-                }
-            };
+        // for (i = 0; i < 5; i++) {
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": cardio,
+        //         "method": "GET",
+        //         "headers": {
+        //             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        //             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+        //         }
+        //     };
 
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            });
-        }
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response[i]);
+        //     });
+        // }
 
         // 5 chest workouts
-        for (i = 0; i < 5; i++) {
-            const settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": chest,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-                    "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-                }
-            };
+        // for (i = 0; i < 5; i++) {
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": chest,
+        //         "method": "GET",
+        //         "headers": {
+        //             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        //             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+        //         }
+        //     };
 
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            });
-        }
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response[i]);
+        //     });
+        // }
 
         // 5 shoulders workouts
-        for (i = 0; i < 5; i++) {
-            const settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": shoulders,
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-                    "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
-                }
-            };
+        // for (i = 0; i < 5; i++) {
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": shoulders,
+        //         "method": "GET",
+        //         "headers": {
+        //             "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        //             "x-rapidapi-key": "de8e14ca10msh892e42abbcd4964p12db82jsnbc1393fd740a"
+        //         }
+        //     };
 
-            $.ajax(settings).done(function (response) {
-                console.log(response[i]);
-            });
-        }
+        //     $.ajax(settings).done(function (response) {
+        //         console.log(response[i]);
+        //     });
+        // }
 
 
 
@@ -163,8 +168,6 @@ function startPage() {
 
 
 startPage();
-// get calorie value
-var calorieInput = document.getElementById("#calories")
 
     //listen for form submit
     // $( "#workOutBtn" ).click(function() {
