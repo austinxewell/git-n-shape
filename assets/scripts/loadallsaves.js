a96e18b3ea4cee555e4dc145a54eb1da0470952c
DerVerruckte <michael.heinhold@gmail.com>
2021-09-07 20:56:09 -0600

    workout saved data

--- assets/scripts/loadallsaves.js
+++ assets/scripts/loadallsaves.js
@@ -3,17 +3,27 @@ var allSavedData = JSON.parse(localStorage.getItem("saves")) || [];
 var clearBtn = document.querySelector('#clear-btn');
 
 var displaySaves = function() {
-    var workouts = allSavedData.workout
+    var workouts = allSavedData[0].workout
+    console.log(workouts);
+    var dayNumber = 1;
+    for (i=0;i<7;i++){
+        var slot = 0;
+        for(var u = 0;u<5;u++){
+            var workingDiv = document.querySelector("#workoutDay"+dayNumber+"-"+slot);
+            workingDiv.textContent =workouts[slot];
+            slot++;
+        }
+        dayNumber++
+    }
 
 
     allSavedData.forEach(element => {
         // var linkEl = document.createElement('a');
         var listItem = document.createElement('li');
 
-        listItem.textContent = element.name+' - '+element.nutrition+element.workout;
+        listItem.textContent = element.name+' - '+element.nutrition;
 
         savedList.appendChild(listItem);
-        
     });
 }
 
