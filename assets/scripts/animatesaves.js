9a36d97a1fb56eedbd907cba10ef3e41efbc15e9
SadboiTay <tg.hakes@gmail.com>
2021-09-08 12:42:14 -0600

    animate plan buttons

--- assets/scripts/animatesaves.js
+++ assets/scripts/animatesaves.js
@@ -1,13 +1,16 @@
 const savedDataCol = document.querySelector("#saved-data-column");
 const planNamesCol = document.querySelector("#saved-plans-column");
 const titleBar = document.querySelector("#plan-title-space");
+const planButton = document.querySelector("#plan");
+
 
 const SavedPlansTL = new TimelineMax();
 
 const introSavedSequence = function () {
     SavedPlansTL.fromTo(savedDataCol, 1.2, {x: "-140%"}, {x: "0%", ease: Power2.easeInOut})
     .fromTo(titleBar, 1.2, {x: "-140%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.3")
-    .fromTo(planNamesCol, 1.2, {x: "-600%"}, {x: "0%", ease: Power2.easeInOut}, "-=1");
+    .fromTo(planNamesCol, 1.2, {x: "-600%"}, {x: "0%", ease: Power2.easeInOut}, "-=1")
+    .staggerFromTo(plan, 1.2, {opacity: 0, y: -50}, {opacity: 1, y: 0, ease: Power2.easeInOut}, 0.1);
 }
 
 introSavedSequence();
